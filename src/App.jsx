44d465167298279ec434650d7a51eaf03c975bcd
khushi-kushwaha <khushikushwaha3009@gmail.com>
2024-09-27 20:30:06 -1200

    use 2-way binding and  preform text validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import About from "./components/About.jsx";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
+
 
 const App = () => {
   // Access the API key from environment variables
@@ -51,12 +52,66 @@ const takeninfo = (info) => {
 
 }
 
+const [title, setTitle] = useState("hello world!");
+
+const getInfo = () =>{
+  setTitle("Hello World! I am clicked");
+}
+
+// console.log(title);
+
+const [ time ,settime] = useState(new Date().toLocaleTimeString());
+
+setInterval(() => {
+    settime(new Date().toLocaleTimeString())
+},1000);
+
+
+const SubmitHandler = (e) => {
+   e.preventDefault();
+   console.log(e.target.username.value);
+   console.log("From SubmitHandler");
+};
+
+
+const [username,setusername] = useState("");
+const submithandlers = (e)=>{
+  e.preventDefault();
+  console.log(username);
+};
+
+
+const[ error ,seterror] =useState(false);
 
   return (
-    <div>
+    <div className="p-3 rounded w-[80%] mx-auto mt-10 bg-zinc-300">
+           
+           <form onSubmit = {submithandlers} >
+            <input type="type" 
+            placeholder="username"
+            name = "username"
+            onInput={(e) => e.target.value.length < 5 
+              ?seterror (true)
+              :seterror (false)
+            }
+            onChange={(e) => setusername(e.target.value)}
+           />
+            {error && <p className="text-red-500">Username length must be greater than 5 characters</p>}
+            <button>Submit</button>
+           </form>
     
-      <button onClick={getinfo} className= "mt-10 mx-10 px-2 py-4 rounded bg-red-300">Print me!</button>
-      <button onClick ={() => takeninfo("This is the info!")}className= "mt-10 mx-10 px-2 py-4 rounded bg-red-300">click me!</button>
+       {/* <form onSubmit={SubmitHandler}>
+        <input name = "username" placeholder="Enter username" className="mt-10 mx-10 px-4 py-2 rounded bg-green-100" onInput="{e.target.value.length < 5? seterror(true) : seterror(false)}"/>
+        <button className = "mt-10  mx-10 px-2 py- 4 rounded bg-blue-200">Submit</button>
+       </form> */}
+
+      {/* <h1 className ="text-center mt-10 text-5xl">{time}</h1> */}
+      {/* <h1>{title}</h1>
+     <button onClick={getInfo} className = "mt-10 mx-10 px-2 py-4 rounded bg-red-300">Click me!</button> */}
+     
+    
+      {/* <button onClick={getinfo} className= "mt-10 mx-10 px-2 py-4 rounded bg-red-300">Print me!</button>
+      <button onClick ={() => takeninfo("This is the info!")}className= "mt-10 mx-10 px-2 py-4 rounded bg-red-300">click me!</button> */}
     </div>
   
 
@@ -64,3 +119,10 @@ const takeninfo = (info) => {
 };
 
 export default App;
+
+
+//useSatet
+//2-way binding 
+//object destructing 
+//validation
+//radio
